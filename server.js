63e4faa2661247ef0cbc7399d6ddab8cdea827c5
josephriosio <blankworldwide@gmail.com>
2019-04-24 15:49:01 -0700

    added helmet and morgan

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const helmet = require("helmet");
-// const morgan = require("morgan");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const userRouter = require("./routes/user-router");
 const postRouter = require("./routes/post-router");
@@ -8,6 +8,8 @@ const postRouter = require("./routes/post-router");
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
 
 server.use("/api/users", userRouter);
 server.use("/api/posts", postRouter);
